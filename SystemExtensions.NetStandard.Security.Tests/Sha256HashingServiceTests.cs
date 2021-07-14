a2e99cb263588f6cdf81c6d18fe08dd9f431acc9
Alexandru Macocian <almacoci@microsoft.com>
2021-07-14 12:17:31 +0300

    SystemExtensions Security

---
+++ SystemExtensions.NetStandard.Security.Tests/Sha256HashingServiceTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using SystemExtensions.NetStandard.Security.Hashing;
+
+namespace VaultO.Tests
+{
+    [TestClass]
+    public sealed class Sha256HashingServiceTests
+    {
+        private readonly IHashingService hashingService = new Sha256HashingService();
+        private string toHashtString;
+        private byte[] toHashtBytes;
+        private Stream toHashStream;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.toHashtBytes = Encoding.UTF8.GetBytes("toEncrypt");
+            this.toHashtString = Convert.ToBase64String(this.toHashtBytes);
+            this.toHashStream = new MemoryStream(this.toHashtBytes);
+        }
+
+        [TestMethod]
+        public async Task HashString()
+        {
+            var hash = await this.hashingService.Hash(this.toHashtString).ConfigureAwait(false);
+            hash.Should().BeOfType<string>();
+        }
+
+        [TestMethod]
+        public async Task HashBytes()
+        {
+            var hash = await this.hashingService.Hash(this.toHashtBytes).ConfigureAwait(false);
+            hash.Should().BeOfType<byte[]>();
+        }
+
+        [TestMethod]
+        public async Task HashStream()
+        {
+            var hash = await this.hashingService.Hash(this.toHashStream).ConfigureAwait(false);
+            hash.Should().BeAssignableTo<Stream>();
+        }
+    }
+}
---
+++ SystemExtensions.NetStandard.Security/Hashing/Sha256HashingService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace SystemExtensions.NetStandard.Security.Hashing
+{
+    public sealed class Sha256HashingService : IHashingService
+    {
+        public async Task<string> Hash(string raw)
+        {
+            using var rawStream = BytesToStream(StringToBytes(raw));
+            using var hashedStream = await HashInternal(rawStream).ConfigureAwait(false);
+            return StreamToString(hashedStream);
+        }
+        public async Task<byte[]> Hash(byte[] raw)
+        {
+            using var rawStream = BytesToStream(raw);
+            using var hashedStream = await HashInternal(rawStream).ConfigureAwait(false);
+            return StreamToBytes(hashedStream);
+        }
+        public async Task<Stream> Hash(Stream raw)
+        {
+            return await HashInternal(raw);
+        }
+
+        private static byte[] StreamToBytes(Stream value)
+        {
+            value.Position = 0;
+            byte[] buffer = new byte[1024];
+            using var ms = new MemoryStream();
+            int read = -1;
+            do
+            {
+                read = value.Read(buffer, 0, buffer.Length);
+                ms.Write(buffer, 0, read);
+            } while (read > 0);
+            return ms.ToArray();
+        }
+        private static string StreamToString(Stream value)
+        {
+            return Convert.ToBase64String(StreamToBytes(value));
+        }
+        private static byte[] StringToBytes(string value)
+        {
+            return Convert.FromBase64String(value);
+        }
+        private static Stream BytesToStream(byte[] value)
+        {
+            return new MemoryStream(value);
+        }
+        private static async Task<Stream> HashInternal(Stream raw)
+        {
+            if (raw is null) throw new ArgumentNullException(nameof(raw));
+
+            using var sha256 = SHA256.Create();
+            return await Task.Run(() => new MemoryStream(sha256.ComputeHash(raw)));
+        }
+    }
+}
