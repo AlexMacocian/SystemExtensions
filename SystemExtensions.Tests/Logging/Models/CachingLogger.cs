8ed9f331d9b8c9e7302a2722c928c95021dbae9d
Alexandru Macocian <amacocian@yahoo.com>
2022-01-16 15:04:16 +0100

    ScopedLogger Logging extensions

---
+++ SystemExtensions.Tests/Logging/Models/CachingLogger.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace SystemExtensions.NetStandard.Tests.Logging.Models
+{
+    public sealed class CachingLogger<T> : ILogger<T>
+    {
+        public List<string> LogCache { get; } = new();
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            var message = formatter(state, exception);
+            this.LogCache.Add(message);
+        }
+    }
+}
