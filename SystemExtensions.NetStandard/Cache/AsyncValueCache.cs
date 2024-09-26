7d065953070f9fcc8fa5f8893323222bc960862a
Alex Macocian <amacocian@yahoo.com>
2024-09-26 10:32:38 +0200

    Implement value caches

---
+++ SystemExtensions.NetStandard/Cache/AsyncValueCache.cs
@@ -0,0 +1,36 @@
+﻿using System.Extensions;
+using System.Threading.Tasks;
+
+namespace System.Cache;
+public sealed class AsyncValueCache<T>(Func<Task<T>> cacheRefreshOperation, TimeSpan cacheDuration)
+    where T : class
+{
+    private readonly Func<Task<T>> cacheRefreshOperation = cacheRefreshOperation.ThrowIfNull(nameof(cacheRefreshOperation));
+    private readonly TimeSpan cacheDuration = cacheDuration;
+
+    private T? value;
+    private DateTime lastCacheRefresh = DateTime.MinValue;
+
+    public async Task<T> GetValue()
+    {
+        if (DateTime.Now - this.lastCacheRefresh < this.cacheDuration &&
+            this.value is not null)
+        {
+            return this.value;
+        }
+
+        return await this.PerformCacheRefresh();
+    }
+
+    public async Task<T> ForceCacheRefresh()
+    {
+        return await this.PerformCacheRefresh();
+    }
+
+    private async Task<T> PerformCacheRefresh()
+    {
+        this.value = await this.cacheRefreshOperation();
+        this.lastCacheRefresh = DateTime.Now;
+        return this.value;
+    }
+}
---
+++ SystemExtensions.NetStandard/Cache/ValueCache.cs
@@ -0,0 +1,35 @@
+﻿using System.Extensions;
+
+namespace System.Cache;
+public sealed class ValueCache<T>(Func<T> cacheRefreshOperation, TimeSpan cacheDuration)
+    where T : class
+{
+    private readonly Func<T> cacheRefreshOperation = cacheRefreshOperation.ThrowIfNull(nameof(cacheRefreshOperation));
+    private readonly TimeSpan cacheDuration = cacheDuration;
+
+    private T? value;
+    private DateTime lastCacheRefresh = DateTime.MinValue;
+
+    public T GetValue()
+    {
+        if (DateTime.Now - this.lastCacheRefresh < this.cacheDuration &&
+            this.value is not null)
+        {
+            return this.value;
+        }
+
+        return this.PerformCacheRefresh();
+    }
+
+    public T ForceCacheRefresh()
+    {
+        return this.PerformCacheRefresh();
+    }
+
+    private T PerformCacheRefresh()
+    {
+        this.value = this.cacheRefreshOperation();
+        this.lastCacheRefresh = DateTime.Now;
+        return this.value;
+    }
+}
